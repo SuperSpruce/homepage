45fd6929bf48cb0b3c4bfe0401d8c887fe111577
SuperSpruce <48099460+SuperSpruce@users.noreply.github.com>
2022-03-14 22:17:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function tab(t) {
+  document.getElementById("tab1").style.display = "none";
+	document.getElementById("tab2").style.display = "none";
+  
+  document.getElementById(t).style.display = "inline-block"
+}
